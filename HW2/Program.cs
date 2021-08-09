62fa0e46856f36ecac0b19245914626e93b6ba65
Ismatullo Mukhamedzhanov <ismatullo.mukhamedzhanov_2022@UCA.corp>
2021-08-09 16:54:46 +0500

    Second commit. Comments removed as requested.

--- HW2/Program.cs
+++ HW2/Program.cs
@@ -10,55 +10,55 @@ namespace HW2Solution
             // Даны две переменные целого типа: A и B. Если их значения не равны, то присвоить
             // каждой переменной большее из этих значений, а если равны, то присвоить переменным
             // нулевые значения. Вывести новые значения переменных A и B.
-            // Console.WriteLine("Enter first number = ");
-            // int a = int.Parse(Console.ReadLine());
-            // Console.WriteLine("Enter second number = ");
-            // int b = int.Parse(Console.ReadLine());
-            // if(a == b)
-            // {
-            //     a = b = 0;
-            // }   
-            // else
-            // {
-            //     if(a>b)
-            //     {
-            //         b = a;
-            //     }
-            //     else
-            //     {
-            //         a = b;
-            //     }
-            // }         
-            // Console.WriteLine($"New valuse of A and B are {a} , {b}");
+            Console.WriteLine("Enter first number = ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number = ");
+            int b = int.Parse(Console.ReadLine());
+            if(a == b)
+            {
+                a = b = 0;
+            }   
+            else
+            {
+                if(a>b)
+                {
+                    b = a;
+                }
+                else
+                {
+                    a = b;
+                }
+            }         
+            Console.WriteLine($"New valuse of A and B are {a} , {b}");
             // Question 3
-            // Random random = new Random();
-            // double rand1 = random.Next(0 , 10000);
-            // double rand2 = random.Next(0 , 10000);
-            // double result;
-            // Console.WriteLine("Enter your sign: ");
-            // string sign = Console.ReadLine();
-            // switch(sign){
-            //     case "+": result = rand1 + rand2; 
-            //     Console.WriteLine($"Your result is {result}");
-            //     break;
-            //     case "-": result = rand1 - rand2;
-            //     Console.WriteLine($"Your difference is {result}");
-            //     break;
-            //     case "*": result = rand1 * rand2;
-            //     Console.WriteLine($"Your multiplication is {result}");
-            //     break;
-            //     case "/":
-            //     string text = rand2 == 0 ? ("Division by 0, not acceptable") : ("Your division is " + (rand1 / rand2).ToString());
-            //     Console.WriteLine(text);
-            //     // case "/":try{
-            //     //     result = rand1 / rand2;
-            //     //     Console.WriteLine($"Your division is {result}");
-            //     // }
-            //     // catch(DivideByZeroException){
-            //     //     Console.WriteLine("Division by zero");
-            //     // }
-            //     break;
-            // }
+            Random random = new Random();
+            double rand1 = random.Next(0 , 10000);
+            double rand2 = random.Next(0 , 10000);
+            double result;
+            Console.WriteLine("Enter your sign: ");
+            string sign = Console.ReadLine();
+            switch(sign){
+                case "+": result = rand1 + rand2; 
+                Console.WriteLine($"Your result is {result}");
+                break;
+                case "-": result = rand1 - rand2;
+                Console.WriteLine($"Your difference is {result}");
+                break;
+                case "*": result = rand1 * rand2;
+                Console.WriteLine($"Your multiplication is {result}");
+                break;
+                case "/":
+                string text = rand2 == 0 ? ("Division by 0, not acceptable") : ("Your division is " + (rand1 / rand2).ToString());
+                Console.WriteLine(text);
+                // case "/":try{
+                //     result = rand1 / rand2;
+                //     Console.WriteLine($"Your division is {result}");
+                // }
+                // catch(DivideByZeroException){
+                //     Console.WriteLine("Division by zero");
+                // }
+                break;
+            }
             // Question4
             Console.WriteLine("Enter number in range from 0 to 100");
             int num = int.Parse(Console.ReadLine());
